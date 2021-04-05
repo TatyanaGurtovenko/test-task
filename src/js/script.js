a089604662d72fa7584a38858e5b6fc8d76a7d73
Tatyana Gurtovenko <t.gurtovenko.sas@gmail.com>
2021-04-05 20:05:50 +0500

    added changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,40 @@
+const selectCount = document.querySelector('.dropdown-select-count');
+const selectName = document.querySelector('.dropdown-select-name');
+const listName = document.querySelector('.dropdown__list-name');
+const listCount = document.querySelector('.dropdown__list-count');
 
-const checkboxBtn = document.querySelector('.input-checkbox__btn');
-checkboxBtn.onclick = function() {
-    
+listName.onclick = function(event) {
+  let target = event.target; 
+
+  if (target.tagName != 'LI') return;
+
+  selectName.textContent = target.textContent;
 };
+selectName.onclick = function(event) {
+  listName.classList.toggle('dropdown__list-onclick');
+};
+listCount.onclick = function(event) {
+  let target = event.target; 
+
+  if (target.tagName != 'LI') return;
+
+  selectCount.textContent = target.textContent;
+};
+selectCount.onclick = function(event) {
+  listCount.classList.toggle('dropdown__list-onclick');
+};
+
+const minus = document.querySelector('.button-minus');
+const plus = document.querySelector('.button-plus');
+const count = document.querySelector('.button-count');
+
+minus.onclick = function() {
+  count.innerHTML--;
+}
+plus.onclick = function() {
+  count.innerHTML++;
+}
+
+
+
+
